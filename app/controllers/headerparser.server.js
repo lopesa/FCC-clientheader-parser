f9974ab3ce66d062ad081bd09ba5db273950c48f
Tony Lopes <tony@lopesdesign.com>
2016-04-09 19:30:23 -0700

    working. final?

--- app/controllers/headerparser.server.js
+++ app/controllers/headerparser.server.js
@@ -3,88 +3,35 @@
 function headerparser () {
    this.parseheader  = function (req, res) {
 
-      var finalReply = {}
+      var finalReply = {},
+         // for heroku's forwarding
+         ip,
+         language,
+         software;
 
-      
+      // for heroku's forwarding
+      ip = req.headers['x-forwarded-for'];
 
-      // res.send('yay farts')
-      res.send(req.ip)
-      // res.send(req.headers)
-   
+      language = req.headers['accept-language']
+         .slice(0, req.headers['accept-language'].indexOf(','));
 
+      software = req.headers['user-agent']
+         .slice(
+            req.headers['user-agent'].indexOf('(') + 1
+            , req.headers['user-agent'].indexOf(')')
+         );
+
+      // for my server setup
+      // finalReply.ipaddress = req.ip;
+
+      // for heroku's forwarding
+      finalReply.ipaddress = ip;
+      finalReply.language = language;
+      finalReply.software = software;
+
+      res.send(finalReply);
+      // res.send(req.headers);
    }
-
-   // this.convert = function (req, res) {
-   //    // res.send(req.params[0])
-   //    var inputDate = req.params[0],
-   //       myDate = new Date(inputDate).toString(),
-   //       UTCDate = new Date(inputDate).toUTCString(),
-   //       timeZoneOffsetSecs, 
-   //       buildDates,
-   //       finalDateObj;
-
-      
-   //    // console.log(myDate);
-   //    // console.log(UTCDate);
-
-      
-   //    buildDates = function buildDates() {
-         
-   //       var dates = {},
-   //          UTCdate,
-   //          UTCmonth,
-   //          UTCyear,
-   //          UTCstring;
-         
-   //       // string case
-   //       if (isNaN(inputDate)) {
-   //          // catches non-date strings
-   //          if (isNaN(Date.parse(inputDate))) {
-   //             dates.natural = null;
-   //             dates.linux = null;
-   //          }
-   //          else {
-   //             timeZoneOffsetSecs = new Date(inputDate).getTimezoneOffset() * 60
-
-   //             dates.natural = new Date(inputDate).toLocaleString(
-   //                'en-US', {
-   //                   // timeZone: 'UTC',
-   //                   month: 'long',
-   //                   day: 'numeric',
-   //                   year: 'numeric'
-   //                }
-   //             );
-   //             dates.linux = new Date(inputDate) / 1000 - timeZoneOffsetSecs;
-   //          }
-   //       }
-
-   //       // number case
-   //       else {
-   //          // catches non date numbers 
-   //          if (isNaN(new Date(Number(inputDate)))) {
-   //             dates.natural = null;
-   //             dates.linux = null;
-   //          }
-   //          else {            
-               
-   //             dates.natural = new Date(Number(inputDate) * 1000).toLocaleString(
-   //                'en-US', {
-   //                   timeZone: 'UTC',
-   //                   month: 'long',
-   //                   day: 'numeric',
-   //                   year: 'numeric'
-   //                }
-   //             );
-   //             dates.linux = inputDate;
-   //          }
-   //       }
-   //       return dates;
-   //    }
-
-   //    finalDateObj = buildDates();
-
-   //    res.send(finalDateObj);
-   // }
 }
 
 module.exports = headerparser;
